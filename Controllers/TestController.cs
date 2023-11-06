c15baf97486f51eefb55f6a807e89b3be4bc01d5
tuchin1228 <jhinwei1228@gmail.com>
2023-11-06 15:58:12 +0800

    加入專案檔案。

---
+++ Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Back1106.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class TestController : ControllerBase
+	{
+		[HttpGet]
+		public IActionResult Get()
+		{
+			return Ok(new
+			{
+				message = "App Service 後端部署"
+			});
+		}
+
+	}
+}
